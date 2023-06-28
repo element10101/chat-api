4a4f604538252436dafeff235be7729cc5afd5f0
element10101 <134858054+element10101@users.noreply.github.com>
2023-06-28 15:42:57 -0500

    Client object

--- chat/__init__.py
+++ chat/__init__.py
@@ -1,1 +1,13 @@
 from requests import get, post
+
+class Client:
+  def __init__(self, api_key: str) -> None:
+    self.api_key = api_key
+  def getTheme(self, user: str) -> str:
+    data = post("https://chat.element1010.repl.co/api/get-theme", json={
+      "apiKey": self.api_key,
+      "user": user
+    })
+    if data.status_code != 200:
+      data.raise_for_status()
+    data = data.json()
