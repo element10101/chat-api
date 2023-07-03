abefa89211e3d382e1eb2e4deac9a035b6ae177f
element10101 <134858054+element10101@users.noreply.github.com>
2023-07-02 19:37:31 -0500

    XSSDetectedError and OwnerBannedError.

--- chat/exceptions.py
+++ chat/exceptions.py
@@ -2,3 +2,13 @@ class APIKeyError(Exception):
   """
   An error for when a given API key is bad.
   """
+
+class XSSDetectedError(Exception):
+  """
+  An error for when XSS is detected in a message.
+  """
+
+class OwnerBannedError(Exception):
+  """
+  An error for when the owner of an API key is banned.
+  """
